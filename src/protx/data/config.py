3155d02cf4987abe128393b7f05b5de6e16d5670
heispv <peymanvahidi1998@gmail.com>
2025-04-30 13:08:21 +0200

    Add path info for the ProtXDataProcessor class

--- src/protx/data/config.py
+++ src/protx/data/config.py
@@ -24,6 +24,7 @@ class Config:
         self.base_dir = Path("output")
         self.raw_dir = self.base_dir / "data/raw"
         self.filter_split_dir = self.base_dir / "data/filter_split"
+        self.protx_dataset_dir = self.base_dir / "data/protx_dataset"
         
         self.uniref50_url = "https://ftp.uniprot.org/pub/databases/uniprot/knowledgebase/complete/uniprot_sprot.fasta.gz"
         self.uniref50_fasta_gz = self.raw_dir / "uniref50.fasta.gz"
@@ -33,3 +34,8 @@ class Config:
         self.train_file = self.filter_split_dir / "train.fasta"
         self.val_file = self.filter_split_dir / "val.fasta"
         self.info_file = self.filter_split_dir / "dataset_info.txt"
+
+        # These will be used if you want to precompute the embeddings
+        # If they are gonna be calculated on-the-fly (suggested), these won't be used
+        self.protx_train = self.protx_dataset_dir / "train.h5"
+        self.protx_val = self.protx_dataset_dir / "val.h5"
