231b3763d693653e0804bc6ccf6c8394cb325041
heispv <peymanvahidi1998@gmail.com>
2025-08-01 18:12:48 +0200

    Refactor download method for improved readability and maintainability.

--- src/protx/data/downloader.py
+++ src/protx/data/downloader.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 
 from ..utils import create_dirs, logger
 
+
 class Downloader:
     """Class for downloading the UniRef50 dataset."""
 
@@ -18,17 +19,22 @@ class Downloader:
     def download(self):
         """Download UniRef50 dataset if it doesn't exist."""
         create_dirs(self.output_file)
-        
+
         if self.output_file.exists():
             logger.info(f"{self.output_file} already exists")
             return
-        
-        with tqdm(unit='B', unit_scale=True, unit_divisor=1024, desc=f"Downloading {self.output_file}") as pbar:
+
+        with tqdm(
+            unit='B',
+            unit_scale=True,
+            unit_divisor=1024,
+            desc=f"Downloading {self.output_file}"
+        ) as pbar:
             def update_progress(block_count, block_size, total_size):
                 if total_size > 0:
                     pbar.total = total_size
                     pbar.update(block_size)
-            
+
             urllib.request.urlretrieve(self.url, self.output_file, reporthook=update_progress)
         
         file_size = Path(self.output_file).stat().st_size
