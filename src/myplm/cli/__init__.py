22e2ee653b1599fcb80a1db4159c7a3399d8c612
heispv <peymanvahidi1998@gmail.com>
2025-09-01 14:21:30 +0200

    fix: running myplm alone won't throw an error

--- src/myplm/cli/__init__.py
+++ src/myplm/cli/__init__.py
@@ -4,16 +4,17 @@ from myplm.cli.data import data
 from myplm.cli.train import train
 
 
-@click.group()
+@click.group(invoke_without_command=True)
 @click.version_option()
-@click.help_option('--help', '-h')
-def cli():
+@click.help_option("--help", "-h")
+@click.pass_context
+def cli(ctx):
     """Make your own protein language model"""
-    pass
+    if ctx.invoked_subcommand is None:
+        click.echo(ctx.get_help())
+        ctx.exit(0)
 
 
 # Attach grouped subcommands
 cli.add_command(data)
 cli.add_command(train)
-
-
