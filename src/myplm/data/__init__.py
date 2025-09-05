835a38100f90efba8324eecefe9367d28758eb23
heispv <peymanvahidi1998@gmail.com>
2025-09-05 18:00:27 +0200

    feat(data): update data module to include KDDataset handling and filtering capabilities]

--- src/myplm/data/__init__.py
+++ src/myplm/data/__init__.py
@@ -1,9 +1,16 @@
 from myplm.data.pipeline import DataPipeline
-from myplm.data.dataset import ProtXDataLoader, ProtXDataLoaderOptimized, ProtXDataProcessor
+from myplm.data.dataset import LoadKDDataset, LoadKDDatasetOptimized, SaveKDDataset, KDDatasetOnTheFly
+from myplm.data.filterer import Filterer, FilterError
+from myplm.data.splitor import Splitor, SplitError
 
 __all__ = [
     "DataPipeline",
-    "ProtXDataLoader", 
-    "ProtXDataLoaderOptimized",
-    "ProtXDataProcessor"
+    "LoadKDDataset", 
+    "LoadKDDatasetOptimized",
+    "SaveKDDataset",
+    "KDDatasetOnTheFly",
+    "Filterer",
+    "FilterError",
+    "Splitor",
+    "SplitError",
 ]
