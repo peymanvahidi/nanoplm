ad1e8d6eb822de0ee9fd87bbd71adb0affb9d6de
heispv <peymanvahidi1998@gmail.com>
2025-09-09 15:57:59 +0200

    refactor: remove unused pretraining components from student model exports

--- src/nanoplm/models/student/__init__.py
+++ src/nanoplm/models/student/__init__.py
@@ -1,36 +1,15 @@
 from nanoplm.models.student.model import ProtX
 from nanoplm.models.student.tokenizer import ProtXTokenizer
 from nanoplm.models.student.feature_embedding import FeatureEmbedding
-from nanoplm.models.student.pretraining import (
-    ProtXMLMConfig,
-    ProtXMLM,
-    ProtXMLMTokenizer,
-    ProteinMLMDataset,
-    MLMDataCollator,
-    MLMTrainer,
-    create_training_args,
-    create_trainer,
-    create_mlm_model_from_config,
-    create_mlm_model_from_checkpoint,
-    save_mlm_model_for_downstream
-)
 
 __all__ = [
     "ProtX",
     "ProtXTokenizer",
     "FeatureEmbedding",
-    # MLM Pretraining components
-    "ProtXMLMConfig",
-    "ProtXMLM",
-    "ProtXMLMTokenizer", 
-    "ProteinMLMDataset",
-    "MLMDataCollator",
-    "MLMTrainer",
     # Hugging Face Trainer utilities
     "create_training_args",
     "create_trainer",
     # Model creation utilities
     "create_mlm_model_from_config",
     "create_mlm_model_from_checkpoint",
-    "save_mlm_model_for_downstream"
 ]
