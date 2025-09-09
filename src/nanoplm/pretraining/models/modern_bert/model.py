c1fdbd8ac4ad8ea77ca026197cb3470abd8b0c87
heispv <peymanvahidi1998@gmail.com>
2025-09-09 17:47:43 +0200

    refactor: introduce ProtModernBertMLMConfig dataclass for model configuration

--- src/nanoplm/pretraining/models/modern_bert/model.py
+++ src/nanoplm/pretraining/models/modern_bert/model.py
@@ -1,35 +1,41 @@
+from dataclasses import dataclass
+
 from transformers import ModernBertConfig, ModernBertForMaskedLM
 from nanoplm.pretraining.models.modern_bert.tokenizer import ProtModernBertTokenizer
+
+@dataclass
+class ProtModernBertMLMConfig:
+    hidden_size: int
+    intermediate_size: int
+    num_hidden_layers: int
+    num_attention_heads: int
+    vocab_size: int = 29
+    mlp_activation: str = "swiglu"
+    mlp_dropout: float = 0.0
+    mlp_bias: bool = False
+    attention_bias: bool = False
+    attention_dropout: float = 0.0
+    classifier_activation: str = "gelu"
 
 class ProtModernBertMLM(ModernBertForMaskedLM):
 
     def __init__(
         self,
-        hidden_size: int,
-        intermediate_size: int,
-        num_hidden_layers: int,
-        num_attention_heads: int,
-        vocab_size: int = 29,
-        mlp_activation: str = "swiglu",
-        mlp_dropout: float = 0.0,
-        mlp_bias: bool = False,
-        attention_bias: bool = False,
-        attention_dropout: float = 0.0,
-        classifier_activation: str = "gelu",
+        config: ProtModernBertMLMConfig
     ):
         self.tokenizer = ProtModernBertTokenizer()
 
         self.config = ModernBertConfig(
-            vocab_size=vocab_size,
-            hidden_size=hidden_size,
-            intermediate_size=intermediate_size,
-            num_hidden_layers=num_hidden_layers,
-            num_attention_heads=num_attention_heads,
-            mlp_dropout=mlp_dropout,
-            mlp_bias=mlp_bias,
-            attention_bias=attention_bias,
-            attention_dropout=attention_dropout,
-            classifier_activation=classifier_activation,
+            vocab_size=config.vocab_size,
+            hidden_size=config.hidden_size,
+            intermediate_size=config.intermediate_size,
+            num_hidden_layers=config.num_hidden_layers,
+            num_attention_heads=config.num_attention_heads,
+            mlp_dropout=config.mlp_dropout,
+            mlp_bias=config.mlp_bias,
+            attention_bias=config.attention_bias,
+            attention_dropout=config.attention_dropout,
+            classifier_activation=config.classifier_activation,
             # Set correct token IDs from our tokenizer
             pad_token_id=self.tokenizer.pad_token_id,
             eos_token_id=self.tokenizer.eos_token_id,
