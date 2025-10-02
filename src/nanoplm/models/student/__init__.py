e5f082d52d2ed1e5609dc0353c55b2a16e56efe5
heispv <peymanvahidi1998@gmail.com>
2025-10-02 13:46:37 +0200

    refactor: remove unused training and model creation utilities from student module

--- src/nanoplm/models/student/__init__.py
+++ src/nanoplm/models/student/__init__.py
@@ -6,10 +6,4 @@ __all__ = [
     "ProtX",
     "ProtXTokenizer",
     "FeatureEmbedding",
-    # Hugging Face Trainer utilities
-    "create_training_args",
-    "create_trainer",
-    # Model creation utilities
-    "create_mlm_model_from_config",
-    "create_mlm_model_from_checkpoint",
 ]
