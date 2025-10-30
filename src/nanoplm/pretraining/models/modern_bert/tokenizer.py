464d8da11c866250d58a06bbd56f1134860943bf
peymanvahidi <peymanvahidi1998@gmail.com>
2025-10-30 11:35:27 +0100

    refactor: update ProtModernBertTokenizer to use PreTrainedTokenizerFast
    - Switched from PreTrainedTokenizer to PreTrainedTokenizerFast for improved performance.
    - Implemented a new vocabulary mapping and tokenizer backend using the Tokenizers library.
    - Enhanced text preprocessing and tokenization methods for better handling of input sequences.

--- src/nanoplm/pretraining/models/modern_bert/tokenizer.py
+++ src/nanoplm/pretraining/models/modern_bert/tokenizer.py
@@ -1,17 +1,21 @@
-import re
-from transformers import PreTrainedTokenizer
+from transformers import PreTrainedTokenizerFast
+from tokenizers import Tokenizer
+from tokenizers.normalizers import Replace
+from tokenizers.pre_tokenizers import Split
+from tokenizers.models import WordLevel
 
 
-class ProtModernBertTokenizer(PreTrainedTokenizer):
+class ProtModernBertTokenizer(PreTrainedTokenizerFast):
     def __init__(
         self,
         unk_token="<unk>",
         pad_token="<pad>",
         eos_token="</s>",
         mask_token="<mask>",
+        **kwargs
     ):
         # Default vocabulary mapping: amino acids + special tokens (pad/eos/unk/mask)
-        self.vocab = {
+        vocab = {
             "A": 4, "L": 5, "G": 6, "V": 7, "S": 8, "R": 9, "E": 10, "D": 11,
             "T": 12, "I": 13, "P": 14, "K": 15, "F": 16, "Q": 17, "N": 18,
             "Y": 19, "M": 20, "H": 21, "W": 22, "C": 23, "X": 24, "B": 25,
@@ -19,45 +23,26 @@ class ProtModernBertTokenizer(PreTrainedTokenizer):
             pad_token: 0, eos_token: 1, unk_token: 2, mask_token: 3,
         }
 
+        tokenizer = Tokenizer(WordLevel(vocab=vocab, unk_token=unk_token))
+
+        tokenizer.normalizer = Replace(r"[UZOB]", "X")
+
+        tokenizer.pre_tokenizer = Split(pattern="", behavior="isolated")
+
         super().__init__(
+            tokenizer_object=tokenizer,
             unk_token=unk_token,
             pad_token=pad_token,
             eos_token=eos_token,
             mask_token=mask_token,
+            model_input_names=["input_ids", "attention_mask"],
+            **kwargs
         )
 
-        self.unk_token_id = self.vocab.get(unk_token)
-        self.pad_token_id = self.vocab.get(pad_token)
-        self.eos_token_id = self.vocab.get(eos_token)
-        self.mask_token_id = self.vocab.get(mask_token)
-
-        self.model_input_names = ["input_ids", "attention_mask"]
-
-        self.id_to_token = {v: k for k, v in self.vocab.items()}
-
-    @property
-    def vocab_size(self) -> int:
-        return len(self.vocab)
-
-    def get_vocab(self):
-        return self.vocab
-
-    def _tokenize(self, text):
-        text = self.preprocess(text)
-        return list(text)
-
-    def _convert_token_to_id(self, token):
-        return self.vocab.get(token, self.unk_token_id)
-
-    def _convert_id_to_token(self, index):
-        if index in self.added_tokens_decoder:
-            return self.added_tokens_decoder[index].content
-        return self.id_to_token.get(index, self.unk_token)
-    
-    def preprocess(self, sequence: str) -> str:
-        seq = (sequence or "").strip().upper()
-        seq = re.sub(r"[UZOB]", "X", seq)
-        return seq
+    def prepare_for_tokenization(self, text, is_split_into_words=False, **kwargs):
+        """Preprocess text before tokenization: strip and uppercase."""
+        text = (text or "").strip().upper()
+        return (text, kwargs)
 
     def build_inputs_with_special_tokens(self, token_ids_0, token_ids_1=None):
         # Append EOS to each sequence; no BOS token used
@@ -71,9 +56,6 @@ class ProtModernBertTokenizer(PreTrainedTokenizer):
         if token_ids_1 and token_ids_1[-1] != self.eos_token_id:
             token_ids_1 = token_ids_1 + [self.eos_token_id]
         return token_ids_0 + token_ids_1
-    
-    def get_vocab(self):
-        return {**self.vocab, **self.get_added_vocab()}
 
     def get_special_tokens_mask(self, token_ids_0, token_ids_1=None, already_has_special_tokens=False):
         if already_has_special_tokens:
