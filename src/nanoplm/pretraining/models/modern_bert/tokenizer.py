c4ef7a0f3c5ff1e32ef90309089b14e30581a8d5
peymanvahidi <peymanvahidi1998@gmail.com>
2025-11-10 11:26:48 +0100

    refactor: improve tokenizer normalization in ProtModernBertTokenizer
    - Updated normalization process to include uppercasing of letters and replacement of specific amino acids.
    - Removed unused methods related to previous tokenizer

--- src/nanoplm/pretraining/models/modern_bert/tokenizer.py
+++ src/nanoplm/pretraining/models/modern_bert/tokenizer.py
@@ -1,6 +1,5 @@
 from transformers import PreTrainedTokenizerFast
-from tokenizers import Tokenizer
-from tokenizers.normalizers import Replace
+from tokenizers import Tokenizer, normalizers, Regex
 from tokenizers.pre_tokenizers import Split
 from tokenizers.models import WordLevel
 from tokenizers.processors import TemplateProcessing
@@ -33,7 +32,12 @@ class ProtModernBertTokenizer(PreTrainedTokenizerFast):
 
         tokenizer = Tokenizer(WordLevel(vocab=vocab, unk_token=unk_token))
 
-        tokenizer.normalizer = Replace(r"[UZOB]", "X")
+        upper_case = [
+            normalizers.Replace(chr(ord("a") + i), chr(ord("A") + i)) for i in range(26)
+        ]
+        unknown_aa = [normalizers.Replace(Regex(r"[UZOB]"), "X")]
+
+        tokenizer.normalizer = normalizers.Sequence(upper_case + unknown_aa)
 
         tokenizer.pre_tokenizer = Split(pattern="", behavior="isolated")
 
@@ -67,90 +71,3 @@ class ProtModernBertTokenizer(PreTrainedTokenizerFast):
             model_input_names=["input_ids", "attention_mask"],
             **kwargs,
         )
-
-    def prepare_for_tokenization(self, text, is_split_into_words=False, **kwargs):
-        """Preprocess text before tokenization: strip and uppercase."""
-        text = (text or "").strip().upper()
-        return (text, kwargs)
-
-    def build_inputs_with_special_tokens(self, token_ids_0, token_ids_1=None):
-        """
-        Build model inputs by adding special tokens.
-
-        Without BOS: seq<eos> for single, seq<eos>seq<eos> for pair
-        With BOS: <bos>seq<eos> for single, <bos>seq<eos><bos>seq<eos> for pair
-        """
-        if self.use_bos_token:
-            if token_ids_1 is None:
-                return [self.bos_token_id] + token_ids_0 + [self.eos_token_id]
-            return (
-                [self.bos_token_id]
-                + token_ids_0
-                + [self.eos_token_id]
-                + [self.bos_token_id]
-                + token_ids_1
-                + [self.eos_token_id]
-            )
-        else:
-            if token_ids_1 is None:
-                return token_ids_0 + [self.eos_token_id]
-            return token_ids_0 + [self.eos_token_id] + token_ids_1 + [self.eos_token_id]
-
-    def get_special_tokens_mask(
-        self, token_ids_0, token_ids_1=None, already_has_special_tokens=False
-    ):
-        """
-        Get mask identifying which tokens are special tokens.
-
-        Returns a list of 0s and 1s, with 1 indicating a special token.
-        """
-        if already_has_special_tokens:
-            return super().get_special_tokens_mask(
-                token_ids_0=token_ids_0,
-                token_ids_1=token_ids_1,
-                already_has_special_tokens=True,
-            )
-
-        if self.use_bos_token:
-            if token_ids_1 is None:
-                return [1] + [0] * len(token_ids_0) + [1]
-            return (
-                [1] + [0] * len(token_ids_0) + [1] + [1] + [0] * len(token_ids_1) + [1]
-            )
-        else:
-            if token_ids_1 is None:
-                return [0] * len(token_ids_0) + [1]
-            return [0] * len(token_ids_0) + [1] + [0] * len(token_ids_1) + [1]
-
-    def create_token_type_ids_from_sequences(self, token_ids_0, token_ids_1=None):
-        """
-        Create token type IDs for sequence pairs.
-
-        Token type IDs are 0 for the first sequence and 1 for the second sequence.
-        """
-        if self.use_bos_token:
-            if token_ids_1 is None:
-                return [0] * (len(token_ids_0) + 2)  # +2 for BOS and EOS
-            return [0] * (len(token_ids_0) + 2) + [1] * (
-                len(token_ids_1) + 2
-            )  # +2 for BOS and EOS each
-        else:
-            if token_ids_1 is None:
-                return [0] * (len(token_ids_0) + 1)  # +1 for EOS
-            return [0] * (len(token_ids_0) + 1) + [1] * (
-                len(token_ids_1) + 1
-            )  # +1 for EOS each
-
-    def save_vocabulary(self, save_directory, filename_prefix=None):
-        import os
-        import json
-
-        if filename_prefix is not None:
-            vocab_file = os.path.join(save_directory, f"{filename_prefix}-vocab.json")
-        else:
-            vocab_file = os.path.join(save_directory, "vocab.json")
-
-        with open(vocab_file, "w", encoding="utf-8") as f:
-            json.dump(self.vocab, f, ensure_ascii=False, indent=2)
-
-        return (vocab_file,)
